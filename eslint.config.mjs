d8c7de5fb98b12dc6d8311f2f4ab40fe824a9929
Renat Mardanov <rent1995@ya.ru>
2025-09-15 01:49:05 +0700

    fix: nx svgr

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,7 +5,11 @@ export default [
   ...nx.configs['flat/typescript'],
   ...nx.configs['flat/javascript'],
   {
-    ignores: ['**/dist'],
+    ignores: [
+      '**/dist',
+      '**/vite.config.*.timestamp*',
+      '**/vitest.config.*.timestamp*',
+    ],
   },
   {
     files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
@@ -17,9 +21,29 @@ export default [
           allow: ['^.*/eslint(\\.base)?\\.config\\.[cm]?[jt]s$'],
           depConstraints: [
             {
-              sourceTag: '*',
-              onlyDependOnLibsWithTags: ['*'],
+              sourceTag: 'type:app',
+              onlyDependOnLibsWithTags: [
+                'type:feature',
+                'type:entity',
+                'type:ui',
+                'type:shared',
+              ],
             },
+            {
+              sourceTag: 'type:feature',
+              onlyDependOnLibsWithTags: [
+                'type:entity',
+                'type:ui',
+                'type:shared',
+              ],
+            },
+            {
+              sourceTag: 'type:entity',
+              onlyDependOnLibsWithTags: ['type:shared'],
+            },
+            { sourceTag: 'type:ui', onlyDependOnLibsWithTags: ['type:shared'] },
+            // sourceTag: '*',
+            // onlyDependOnLibsWithTags: ['*'],
           ],
         },
       ],
